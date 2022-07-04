59b4def32e84a222034db4818ea4434eb505d178
alejandroguerravillegas <coreartalex2@gmail.com>
2022-07-04 09:29:56 -0700

    Adding new Account

--- frontend/js/helpers/Common.js
+++ frontend/js/helpers/Common.js
@@ -1,13 +1,1 @@
 //common functions that can be used in different cases
-
-$(document).ready(() => {
-    // Creating new Accounts
-    $('button#newAccountBtn').on('click', () => {
-        console.log('New Account added');
-        const accountSummary = document.getElementById('accountSummary');
-        const newAccount = document.createElement('li');
-        newAccount.className = 'account';
-        newAccount.id = "account";
-        accountSummary.appendChild(newAccount);
-      });
-});
