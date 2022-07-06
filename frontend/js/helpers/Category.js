1e438ac05dd25af43797a8daf568576412686948
DaweUrbi <urban.da@icloud.com>
2022-07-05 20:30:41 -0700

    Category - html + hide input and btn

--- frontend/js/helpers/Category.js
+++ frontend/js/helpers/Category.js
@@ -1,1 +1,9 @@
+$(() => {
+  
+    const categorySelect = $("#categorySelect");
+    const categoryInput = $("#categoryInput");
+    const newCategoryButton = $("#categoryBtn");
+    categoryInput.hide();
+    newCategoryButton.hide();
 
+});
