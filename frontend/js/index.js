02b67877b59d39e0d0c073b5d8be82b3cbed8f88
DaweUrbi <urban.da@icloud.com>
2022-07-05 19:24:55 -0700

    Transaction validation

--- frontend/js/index.js
+++ frontend/js/index.js
@@ -1,5 +1,5 @@
 $(() => {
-    // Submiting form
+    //Submiting form
     $("form").submit((event) => {
         event.preventDefault();
         });
@@ -20,54 +20,68 @@ $(() => {
     }})
     
     // Getting value from radio btn - transaction
-    $(document).ready(function(){
-        $("input[type='radio']").click(function(){
-            let radioValue = $("input[name='transaction']:checked").val();
-            if(radioValue === "deposit"){
-                let deposit = radioValue;
-                console.log(deposit);
-            } else if (radioValue === "withdraw") {
-                let withdraw = radioValue;
-                console.log(withdraw);
-            } else {
-                let transfer = radioValue;
-                console.log(transfer);
-            };
-        });
-    });
-    // Adding transaction 
-    $('#addTransaction').on('click', (e) => {
-        console.log("test");
-        // let accountId = $('#accountId').val();
-        let transactionType = $('input[name="transaction"]:checked').val();
-        let description = $('#transactionDesription').val();
-        let amount = $('#transactionAmount').val();
+    // $(document).ready(function(){
+    //     $("input[type='radio']").click(function(){
+    //         let radioValue = $("input[name='transaction']:checked").val();
+    //         if(radioValue === "deposit"){
+    //             let deposit = radioValue;
+    //             console.log(deposit);
+    //         } else if (radioValue === "withdraw") {
+    //             let withdraw = radioValue;
+    //             console.log(withdraw);
+    //         } else {
+    //             let transfer = radioValue;
+    //             console.log(transfer);
+    //         };
+    //     });
+    // });
+    // // Adding transaction 
+    // $('#addTransaction').on('click', (e) => {
+    //     console.log("test");
+    //     // let accountId = $('#accountId').val();
+    //     let transactionType = $('input[name="transaction"]:checked').val();
+    //     let description = $('#transactionDesription').val();
+    //     let amount = $('#transactionAmount').val();
 
-        transactionDataJSON = JSON.stringify({
-            newTransaction: {
-                accountId: '',
-                transactionType: transactionType,
-                accountIdFrom: '',
-                accountIdTo: '',
-                category: '',
-                description: description,
-                amount: amount,
-            },
-        });
-        $.ajax({
-            method: 'post',
-            data: transactionDataJSON,
-            url: 'http://localhost:3000/transaction',
-            dataType: 'json',
-            contentType: 'application/json',
-        }).done((data) => {
-            console.log(transactionDataJSON);
-        });
-    });
+    //     transactionDataJSON = JSON.stringify({
+    //         newTransaction: {
+    //             accountId: '',
+    //             transactionType: transactionType,
+    //             accountIdFrom: '',
+    //             accountIdTo: '',
+    //             category: '',
+    //             description: description,
+    //             amount: amount,
+    //         },
+    //     });
+    //     $.ajax({
+    //         method: 'post',
+    //         data: transactionDataJSON,
+    //         url: 'http://localhost:3000/transaction',
+    //         dataType: 'json',
+    //         contentType: 'application/json',
+    //     }).done((data) => {
+    //         console.log(transactionDataJSON);
+    //     });
+    // });
 
-
-
+    $("#transactionForm").submit((event) => {
+        event.preventDefault();
+        if($("#transactionDesription").val() === '' & $("#transactionAmount").val() === '') {
+            alert("Please enter a description and amount!");
+            return;
+        } else if( $("#transactionDesription").val() === '') {
+            alert("Please enter an description!");
+            return;
+        } else if($("#transactionAmount").val() === '') {
+            alert("Please enter an amount!");
+            return; }
     
+      });
+
+
+
+
 });
 
 
