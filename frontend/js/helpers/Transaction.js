ebe5d1c45a7467f1be34ffdba0305c7e19aac654
DaweUrbi <urban.da@icloud.com>
2022-07-06 16:46:01 -0700

    transaction validation

--- frontend/js/helpers/Transaction.js
+++ frontend/js/helpers/Transaction.js
@@ -22,18 +22,18 @@ class Deposit extends Transaction {
   }
 }
 
-// Validation input - used in index.js for now
-// $(() => {
-//   $("#transactionForm").submit((e) => {
-//     e.preventDefault();
-//     if($("#transactionDesription").val() === '' & $("#transactionAmount").val() === '') {
-//         alert("Please enter a description and amount!");
-//         return;
-//     } else if( $("#transactionDesription").val() === '') {
-//         alert("Please enter an description!");
-//         return;
-//     } else if($("#transactionAmount").val() === '') {
-//         alert("Please enter an amount!");
-//         return; }
-//     });
-// });
+// Validation empty input
+$(() => {
+  $("#transactionForm").submit((e) => {
+    e.preventDefault();
+    if($("#transactionDesription").val() === '' & $("#transactionAmount").val() === '') {
+        alert("Please enter a description and amount!");
+        return;
+    } else if( $("#transactionDesription").val() === '') {
+        alert("Please enter an description!");
+        return;
+    } else if($("#transactionAmount").val() === '') {
+        alert("Please enter an amount!");
+        return; }
+    });
+});
