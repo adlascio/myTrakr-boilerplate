24a6084e2a55cb504b38ebd5c3bb500e6bd10293
alejandroguerravillegas <coreartalex2@gmail.com>
2022-07-09 07:43:56 -0700

    Fixing Account

--- frontend/js/helpers/Account.js
+++ frontend/js/helpers/Account.js
@@ -13,45 +13,46 @@ class Account {
 
  //-------------------------------------
 $(document).ready(() => {
-  // Event Listener for when we submit the New Account Form
   $('form#newAccount').submit((e)=>{
     e.preventDefault();
-    // Validate that input isnt empty
+    //Validate the input isnt empty
     if($('#inputnewAccount').val() !== ''){
-      // GET the New Accounts
-      $.ajax({
-        method: 'get',
-        url: 'http://localhost:3000/accounts',
-        contentType: 'application/json',
-        dataType:'json',
-      }).done(()=>{
-        console.log('Data Obtained');
-      });
-      // Create the New Account
-      let newAccount = new Account($('#inputnewAccount').val());
+      let input = $('#inputnewAccount').val();
+      let id = 'username';
+      localStorage.setItem(input,id);
+      let newAccount = new Account(input);
+      let usrName = newAccount.username;
+      let tranS = newAccount.transactions;
       console.log(newAccount);
-      console.log('Account Added');
-      // Ad New Account to Summary
-      $('ul#accountSummary').append('<li>'+'Account: '+newAccount.username+'Transactions: '+newAccount.transactions+'</li>');
-      // Post the New Accounts
-      let usrname = newAccount.username;
-      let trans = newAccount.transactions;
+      console.log('Account Created');
+
+      //Post the Accounts on localhost
       $.ajax({
-        method: 'post',
-        data: JSON.stringify({
-          newAccount:{
-            username: `${usrname}`,
-            transactions: `${trans}`,
-          },
-        }),
-        url: 'http://localhost:3000/accounts',
-        contentType: 'application/json',
-        dataType: 'json',
-      }).done(() => {
+      method: 'post',
+      data: JSON.stringify({
+        newAccount:{
+        username: `${usrName}`,
+        ransactions: `${tranS}`,
+        },
+      }),
+      url: 'http://localhost:3000/accounts',
+      contentType: 'application/json',
+      dataType: 'json',
+      }).done((data) => {
+        $('ul#accountSummary').append('<li>'+'Account: '+usrName+'Ttransacions: '+tranS+'</li>');
+        console.log('Accounts Posted');
       });
     }else{
       alert('Please enter an Account');
     }
   });
- 
+  //Post the Accounts in the Transaction fields
+  for(let i = 0; i < localStorage.length; i++){
+    key = localStorage.key(i);
+    $('#accountSelect').append('<option value=>"'+key+'</option>');
+    $('#fromSelect').append('<option value=>"'+key+'</option>');
+    $('#toSelect').append('<option value=>"'+key+'</option>');
+    console.log('Accounts Posted');
+  }
+
 });
