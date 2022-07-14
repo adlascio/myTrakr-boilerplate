093ae24a82942678a561236cf51c3e224a249ad4
alejandroguerravillegas <coreartalex2@gmail.com>
2022-07-14 07:17:21 -0700

    Balance in Summary

--- frontend/js/helpers/Account.js
+++ frontend/js/helpers/Account.js
@@ -14,17 +14,23 @@ class Account {
  //-------------------------------------
 $(document).ready(() => {
   //get accounts
+  const accountSelect = $('#accountSelect');
+  const fromSelect = $('#fromSelect');
+  const toSelect = $('#toSelect');
+  const filterSelect = $("#filterSelect");
+
   $.ajax({
     method:'get',
     url:'http://localhost:3000/accounts',
     contentType:'application/json'
   }).done((data) => {
     console.log('data get accounts',data)
-    let categoryOption = $.map(data, (item) => {
-      $('ul#accountSummary').append('<li>'+'Account: '+item.username+'Transactions:'+item.transactions,'</li>');
-      $('#accountSelect').append('<option value=>'+item.username+'</option>');
-      $('#fromSelect').append('<option value=>'+item.username+'</option>');
-      $('#toSelect').append('<option value=>'+item.username+'</option>');
+    let accountOptions = $.map(data, (item) => {
+      $('ul#accountSummary').append(`<li> Account: ${item.username} Balance: ${item.balance} </li>`);
+      $('#accountSelect').append(`<option value=${item.id}>${item.username}</option>`);
+      $('#fromSelect').append(`<option value=${item.id}>${item.username}</option>`);
+      $('#toSelect').append(`<option value=${item.id}>${item.username}</option>`);
+      $('#filterSelect').append(`<option value=${item.id}>${item.username}</option>`);
     });
   });
   
@@ -33,21 +39,19 @@ $(document).ready(() => {
     //Validate the input isnt empty
     if($('#inputnewAccount').val() !== ''){
       let input = $('#inputnewAccount').val();
-      let id = 'username';
-      // localStorage.setItem(input,id);
       let newAccount = new Account(input);
       let usrName = newAccount.username;
-      let tranS = newAccount.transactions;
+      let balance = newAccount.balance;
       console.log(newAccount);
       console.log('Account Created');
 
-      //Post the Accounts on localhost
+      //Post the Accounts to server
       $.ajax({
       method: 'post',
       data: JSON.stringify({
         newAccount:{
         username: `${usrName}`,
-        transactions: [],
+        balance: `${balance}`,
         },
       }),
       url: 'http://localhost:3000/accounts',
@@ -55,10 +59,15 @@ $(document).ready(() => {
       dataType: 'json',
       }).done((data) => {
         console.log('Accounts Posted');
+        accountSelect.append(`<option value=${data.id}>${data.username}</option>`);
+        fromSelect.append(`<option value=${data.id}>${data.username}</option>`);
+        toSelect.append(`<option value=${data.id}>${data.username}</option>`);
+        filterSelect.append(`<option value=${data.id}>${data.username}</option>`);
+
+        $('#inputnewAccount').val('');
       });
     }else{
       alert('Please enter an Account');
     }
   });
-
 });
--- frontend/js/helpers/Transaction.js
+++ frontend/js/helpers/Transaction.js
@@ -22,7 +22,15 @@ class Deposit extends Transaction {
   }
 }
 
-// Validation of Transaction fields
+class Transfer extends Transaction{
+  constructor(accountIdFrom,accountIdTo){
+    super(Transaction);
+    this.accountIdFrom = accountIdFrom;
+    this.accountIdTo = accountIdTo;
+  }
+}
+
+// Validation of Transaction fields & Transfer
 $(() => {
   $("#transactionForm").submit((e) => {
     e.preventDefault();
@@ -38,16 +46,55 @@ $(() => {
       }else if($("#transactionAmount").val() <= 0){
         alert('Please enter an Amount Grather than 0');
         return;
-      }else if($('#transferInput').val()){
-        //Pending to add code here ... I run out of time, I'll add it tonight
-        // Just in case you pull the code before I get home :)
+      }else if($('#transactionAmount').val() > Account.balance){
+        alert('Insuficient Balance to Complete this Transaction, Please make a Deposit First');
+      }else if($('#fromSelect').val() === $('#toSelect').val()){
+        alert('Accounts Must be Different in order to Proceed with this Transaction');
       }
     });
-});
+    
+    // Adding new transaction 
+    $('#addTransaction').click((e) => {
+      e.preventDefault();
+      let transactionType = $('input[name="transaction"]:checked').val();
+      let description = $('#transactionDesription').val();
+      let amount = $('#transactionAmount').val();
+      let category = $('#transactionCategory').val();
+      let account = $('#accountSelect').val();
+      let fromAccount = $('#fromSelect').val();
+      let toAccount = $('#toSelect').val();
 
-class Transfer extends Transaction{
-  constructor(accountIdFrom,accountIdTo){
-    this.accountIdFrom = accountIdFrom;
-    this.accountIdTo = accountIdTo;
-  }
-}
+      $.ajax({
+          url: "http://localhost:3000/transaction", 
+          type: "post",
+          contentType: "application/json",
+          dataType: "json",
+          data: JSON.stringify({
+              newTransaction: {
+                  transactionType: transactionType,
+                  description: description,
+                  amount: amount,
+                  category: category,
+                  accountId: account,
+                  accountIdFrom: fromAccount,
+                  accountIdTo: toAccount,
+              },
+          }),
+      }).done((data) => {
+          console.log(data);
+          $("#transactionsTable").append(
+              `<tr>
+                  <td>${data[0].id}</td> 
+                  <td>${data[0].accountId}</td> 
+                  <td>${data[0].transactionType}</td> 
+                  <td>${data[0].category}</td> 
+                  <td>${data[0].description}</td>
+                  <td>${data[0].amount}</td> 
+                  <td>${data[0].accountIdFrom == 0 ? `-` : data[0].accountIdFrom}</td> 
+                  <td>${data[0].accountIdTo == 0 ? `-` : data[0].accountIdTo}</td>'
+             </tr>`
+            );
+      });
+
+  });
+});
